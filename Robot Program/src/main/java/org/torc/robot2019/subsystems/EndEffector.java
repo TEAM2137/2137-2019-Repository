a74d1e1ef4244c166eb76da820421e5d9db9c9fe
Conlan Houston <conlan.houston@gmail.com>
2019-04-04 18:32:42 -0400

    Motion Magic C/A Velocity Change

--- Robot Program/src/main/java/org/torc/robot2019/subsystems/EndEffector.java
+++ Robot Program/src/main/java/org/torc/robot2019/subsystems/EndEffector.java
@@ -86,8 +86,8 @@ public class EndEffector extends Subsystem implements InheritedPeriodic {
 		endEffectorM.config_kD(0, 0);
 		endEffectorM.config_IntegralZone(0, 0);//(int)KMap.GetKNumeric(KNumeric.INT_END_EFFECTOR_KIZONE));
 
-		endEffectorM.configMotionCruiseVelocity(200);
-		endEffectorM.configMotionAcceleration(200);
+		endEffectorM.configMotionCruiseVelocity(100);
+		endEffectorM.configMotionAcceleration(100);
 
 		rollerM = new VictorSPX(_rollerMID);
 
