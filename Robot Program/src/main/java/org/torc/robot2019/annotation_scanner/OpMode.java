ec8afbb7cf1e7e13b7e618530f7517f83dc3f661
Wyatt Ashley <wyattashley4809@gmail.com>
2019-07-25 13:00:47 -0400

    Updated Multi Threading

--- Robot Program/src/main/java/org/torc/robot2019/annotation_scanner/OpMode.java
+++ Robot Program/src/main/java/org/torc/robot2019/annotation_scanner/OpMode.java
@@ -1,20 +1,52 @@
 package org.torc.robot2019.annotation_scanner;
 
 public abstract class OpMode extends RobotStateTracker implements Runnable {
+    private int debugLevel = 5;
+
     public abstract void run();
 
     public boolean opModeIsActive(){
         return runningThread.isAlive();
     }
 
-    public void waitForBooleanEvent(OnEvent onEvent){
+    public void waitForBooleanEvent(OnBooleanEvent onEvent){
         long runTime = System.currentTimeMillis();
         
         /*Loop until user sends true and while doing this call a function a user defines*/
         while(!onEvent.waitForEvent(runTime));
     }
 
-    public interface OnEvent {
+    public void waitForEqualDoubleEvent(OnDoubleEvent onEvent, double goal){
+        long runTime = System.currentTimeMillis();
+
+        while(onEvent.waitForEvent(runTime) == goal);
+    }
+
+    public void waitForLessDoubleEvent(OnDoubleEvent onEvent, double goal){
+        long runTime = System.currentTimeMillis();
+
+        while(onEvent.waitForEvent(runTime) >= goal);
+    }
+
+    public void waitForGreaterDoubleEvent(OnDoubleEvent onEvent, double goal){
+        long runTime = System.currentTimeMillis();
+
+        while(onEvent.waitForEvent(runTime) <= goal);
+    }
+
+    public interface OnBooleanEvent {
         public abstract boolean waitForEvent(long runTime);
+    }
+
+    public interface OnDoubleEvent {
+        public abstract double waitForEvent(long runTime);
+    }
+
+    public int getRobotDebug(){
+        return debugLevel;
+    }
+
+    public void setRobotDebug(int debugLevel){
+        this.debugLevel = debugLevel;
     }
 }
--- Robot Program/src/main/java/org/torc/robot2019/opModes/Autonomous2019.java
+++ Robot Program/src/main/java/org/torc/robot2019/opModes/Autonomous2019.java
@@ -1,13 +1,24 @@
 package org.torc.robot2019.opModes;
 
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import org.torc.robot2019.annotation_scanner.OpMode;
 import org.torc.robot2019.annotation_scanner.annotations.Autonomous;
+import org.torc.robot2019.functions.FileLogger;
+import org.torc.robot2019.hardware.BaseDrive;
 
 @Autonomous(name = "Autonomous")
 public class Autonomous2019 extends OpMode{
 
+    //Hardware Objects
+    public BaseDrive baseDrive = new BaseDrive();
+    public FileLogger fileLogger = new FileLogger(5);
+
     @Override
     public void run() {
+    
+        fileLogger.
+        baseDrive.init(fileLogger, 10, 12, 11, 13, MotorType.kBrushless);
         
         while(opModeIsActive()){
             
