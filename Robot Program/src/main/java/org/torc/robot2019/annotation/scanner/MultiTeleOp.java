60b28369e8ae65066226e18f7824de588cf0d525
Wyatt Ashley <wyattashley4809@gmail.com>
2019-11-02 20:03:21 -0500

    Fixed Adding Commands

--- Robot Program/src/main/java/org/torc/robot2019/annotation/scanner/MultiTeleOp.java
+++ Robot Program/src/main/java/org/torc/robot2019/annotation/scanner/MultiTeleOp.java
@@ -1,6 +1,9 @@
 package org.torc.robot2019.annotation.scanner;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -8,10 +11,15 @@ import java.util.concurrent.Future;
 import org.torc.robot2019.opmodes.Autonomous;
 import org.torc.robot2019.opmodes.Disabled;
 import org.torc.robot2019.opmodes.TeleOp;
+import org.torc.robot2019.robot.InheritedPeriodic;
 import org.torc.robot2019.robot.Main;
 
 import edu.wpi.first.hal.HAL;
+import edu.wpi.first.hal.NotifierJNI;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
@@ -20,6 +28,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 @SuppressWarnings("all")
 public class MultiTeleOp extends RobotBase {
+
+    private Watchdog m_watchdog;
+    
+    public boolean commandEnabled = false;
 
     public enum RobotState {
         Autonomous("Autonomous"), TeleOp("TeleOp"), Test("Test"), Disabled("Disabled");
@@ -48,6 +60,10 @@ public class MultiTeleOp extends RobotBase {
     @Override
     public void startCompetition() {
 
+        double peroid = 0.02;
+        m_watchdog = new Watchdog(peroid, this::printLoopOverrunMessage);
+
+        // Tell the DS that the robot is ready to be enabled
         HAL.observeUserProgramStarting();
 
         sendHALInfo();
@@ -79,11 +95,17 @@ public class MultiTeleOp extends RobotBase {
         this.robotSendableCurrent = this.functionSendable.getSelected();
 
         while (!Thread.interrupted()) {
+            m_watchdog.reset();
+
             this.robotStateCurrent = getRobotState();
             this.robotSendableCurrent = functionSendable.getSelected();
 
             LiveWindow.setEnabled(false);
             Shuffleboard.disableActuatorWidgets();
+
+            if(commandEnabled){
+                commandPeriodic();
+            }
 
             if (robotStateCurrent != robotStatePassed || this.robotSendableCurrent != this.robotSendablePassed) {
                 future.cancel(true);
@@ -106,12 +128,46 @@ public class MultiTeleOp extends RobotBase {
                 }
             }
 
+            if(robotStateCurrent == RobotState.Disabled){
+                if(robotStatePassed != RobotState.Disabled){
+                    m_watchdog.addEpoch("disabledInit()");
+                }
+
+                m_watchdog.addEpoch("disablePeriodic()");
+            } else if (robotStateCurrent == RobotState.Autonomous){
+                if(robotStatePassed != RobotState.Autonomous){
+                    m_watchdog.addEpoch("autonomousInit()");
+                }
+
+                m_watchdog.addEpoch("autonomousPeriodic()");
+            } else if (robotStateCurrent == RobotState.TeleOp){
+                if(robotStatePassed != RobotState.TeleOp){
+                    m_watchdog.addEpoch("teleopInit()");
+                }
+
+                m_watchdog.addEpoch("teleopPeriodic()");
+            } else {
+                if(robotStatePassed != RobotState.Test){
+                    m_watchdog.addEpoch("testInit()");
+                }
+
+                m_watchdog.addEpoch("testPeriodic()");
+            }
+
             sendHALInfo();
+
+            m_watchdog.addEpoch("robotPeriodic()");
+            m_watchdog.disable();
 
             SmartDashboard.updateValues();
             LiveWindow.updateValues();
             Shuffleboard.update();
 
+
+            // Warn on loop time overruns
+            if (m_watchdog.isExpired()) {
+                m_watchdog.printEpochs();
+            }
 
             this.robotSendablePassed = this.robotSendableCurrent;
             this.robotStatePassed = this.robotStateCurrent;
@@ -139,5 +195,24 @@ public class MultiTeleOp extends RobotBase {
         } else {
             HAL.observeUserProgramDisabled();
         }
+    }
+
+    private void printLoopOverrunMessage() {
+        DriverStation.reportWarning("Loop time of " + 0.02 + "s overrun\n", false);
+    }
+
+    private static ArrayList<InheritedPeriodic> PeriodicList = new ArrayList<InheritedPeriodic>();
+
+	public static void AddToPeriodic(InheritedPeriodic toAdd) {
+		PeriodicList.add(toAdd);
+	}
+
+    private void commandPeriodic(){
+        // Manually call the Scheduler "run" command.
+		Scheduler.getInstance().run();
+		// Call all perodic funtions in PeriodicList
+		for(InheritedPeriodic per : PeriodicList) {
+			per.Periodic();
+		}
     }
 }
